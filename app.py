ab10f9c83db2f63fa04a6f125d5068cc32506bb8
Vrishank Sai <vrishank@Mario.local>
2024-09-21 20:34:00 +0400

    added option to find one on mongodb database

--- app.py
+++ app.py
@@ -53,6 +53,20 @@ def view_collection():
 
     return render_template('view.html', data=result)
     
+@app.route('/get_user/<name>', methods=['GET'])
+def get_user(name):
+    # Find user by name
+    user_data = collection.find_one({'name': name})
+
+    if user_data:
+         user_data.pop('_id', None)  # Remove MongoDB's internal _id field for cleaner rendering
+    else:
+         user_data = {"error": "User not found"}
+    
+    # Render the user data in the template
+    return render_template('user_rec.html', user_data=user_data)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
